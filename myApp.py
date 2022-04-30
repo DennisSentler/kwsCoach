adec2821b2fd3ab6d3d84ba49f1cd1a47926ac0e
Dennis Sentler <dennis.sent.93@gmail.com>
2022-04-30 18:01:27 +0200

    implemented scrip into Qprocess

--- myApp.py
+++ myApp.py
@@ -1,13 +1,12 @@
 # system modules
-from locale import normalize
 from logging import root
-import sys, os, re
-from numpy import TooHardError, average
+import sys
+import os
+import re
+import yaml
 from pyparsing import col
 # foreign modules
-import librosa
 import qdarkstyle
-from ast import literal_eval
 
 from PyQt5.QtWidgets import (
     QApplication, QMainWindow, QDialog, QFileDialog, QTreeWidgetItem, QSizePolicy, QMessageBox, QFileSystemModel, QMenu, QAction
@@ -28,20 +27,24 @@ from addNewWordDialogImpl import AddNewWordDialog
 from connectProviderDialogImpl import (
     ConnectGoogleDialog, ConnectWatsonDialog, ConnectAzureDialog
 )
+from trainingScriptWrapper import TrainingScriptWrapper
 from voiceTestDialogImpl import VoiceTestDialog
 import tts
+
 
 class MyApp(QMainWindow, Ui_myApp):
     def __init__(self, parent=None):
         super().__init__(parent)
         self.setupUi(self)
         QIcon.setThemeName("ubuntu-mono-dark")
-        ## setup synthesis page
+        # setup synthesis page
         # create voice list view
         default_directory = os.path.abspath(os.path.dirname(__file__))
         self.path_input_text.setText(default_directory)
-        self.dataset_src_path_input_text.setText(str(default_directory)+"/synthesis")
-        self.dataset_dest_path_input_text_2.setText(str(default_directory)+"/synthesis")
+        self.dataset_src_path_input_text.setText(
+            str(default_directory)+"/synthesis")
+        self.dataset_dest_path_input_text_2.setText(
+            str(default_directory)+"/synthesis")
         self.voice_list_view = VoiceListView(parent=self)
         self.voice_list_view.setObjectName("voice_list_view")
         self.gridLayout_4.addWidget(self.voice_list_view, 1, 0, 1, 5)
@@ -50,11 +53,21 @@ class MyApp(QMainWindow, Ui_myApp):
         self._tts = tts.TTSServiceHandler(default_directory)
         self._words_to_synthesize = []
 
-        ## setup argumentation page
+        # setup argumentation page
         # create toggle slider
         self.slider_dataset = Toggle(parent=self)
-        self.slider_dataset.stateChanged['int'].connect(self.toggleDatasetDirectory)
-        self.horizontalLayout_10.insertWidget(1,self.slider_dataset)
+        self.slider_dataset.stateChanged['int'].connect(
+            self.toggleDatasetDirectory)
+        self.horizontalLayout_10.insertWidget(1, self.slider_dataset)
+
+        # setup training page
+        self.trainer = None
+        self._words_to_train = []
+        # load architectures from config file
+        self.architectures = self.loadModelArchitectures()
+        for architecture in self.architectures:
+            self.model_architecture_combobox.addItem(
+                architecture["display_name"])
 
     # menu bar
     def switchToSynthesis(self):
@@ -94,7 +107,8 @@ class MyApp(QMainWindow, Ui_myApp):
                 self.voice_list_view.addVoices(diag.voices)
                 if len(diag.voices) > 0:
                     self.connect_button_google.setEnabled(False)
-            self.status_label_google.setText("Connected" if diag.isConnected() else "Dicsonnected")
+            self.status_label_google.setText(
+                "Connected" if diag.isConnected() else "Dicsonnected")
         except Exception:
             ErrorMessageBox(self).exec()
 
@@ -106,7 +120,8 @@ class MyApp(QMainWindow, Ui_myApp):
                 self.voice_list_view.addVoices(diag.voices)
                 if len(diag.voices) > 0:
                     self.connect_button_watson.setEnabled(False)
-            self.status_label_watson.setText("Connected" if diag.isConnected() else "Dicsonnected")
+            self.status_label_watson.setText(
+                "Connected" if diag.isConnected() else "Dicsonnected")
         except Exception:
             ErrorMessageBox(self).exec()
 
@@ -118,7 +133,8 @@ class MyApp(QMainWindow, Ui_myApp):
                 self.voice_list_view.addVoices(diag.voices)
                 if len(diag.voices) > 0:
                     self.connect_button_azure.setEnabled(False)
-            self.status_label_azure.setText("Connected" if diag.isConnected() else "Dicsonnected")
+            self.status_label_azure.setText(
+                "Connected" if diag.isConnected() else "Dicsonnected")
         except Exception:
             ErrorMessageBox(self).exec()
 
@@ -145,12 +161,12 @@ class MyApp(QMainWindow, Ui_myApp):
         dialog = AddNewWordDialog(self, all_languages)
         if dialog.exec():
             self._words_to_synthesize.append(
-                Word(dialog.word, 
-                    dialog.selected_languages, 
-                    is_random=dialog.random_word_radioButton.isChecked(),
-                    quantity=dialog.random_word_quantity_dial.value()
-                    )
-                )
+                Word(dialog.word,
+                     dialog.selected_languages,
+                     is_random=dialog.random_word_radioButton.isChecked(),
+                     quantity=dialog.random_word_quantity_dial.value()
+                     )
+            )
             self.refreshWordListWidget()
 
     def removeWordFromList(self):
@@ -166,7 +182,8 @@ class MyApp(QMainWindow, Ui_myApp):
             word_item = QTreeWidgetItem(self.words_list_widget)
             word_item.setData(0, 0, index+1)
             if word.is_random:
-                word_item.setData(1, 0, f"...{word.text[-15:]} (qty: {word.quantity})")
+                word_item.setData(
+                    1, 0, f"...{word.text[-15:]} (qty: {word.quantity})")
             else:
                 word_item.setData(1, 0, word.text)
             word_item.setData(2, 0, str(word.languages))
@@ -181,26 +198,30 @@ class MyApp(QMainWindow, Ui_myApp):
         words = self._words_to_synthesize
         self.status_bar.clearMessage()
         if (len(words) == 0 or len(voices) == 0):
-            self.status_bar.showMessage("condition not met! please select at least one voice and one word!", 5000)
+            self.status_bar.showMessage(
+                "condition not met! please select at least one voice and one word!", 5000)
             return
         synthesis = SynthesisDialog(self, words, voices, self._tts)
         synthesis.exec()
 
     # augmentation page
     def openSrcDatasetDirectorySelectDialog(self):
-        dir = QFileDialog.getExistingDirectory(self, 'Select Source Dataset Directory')
+        dir = QFileDialog.getExistingDirectory(
+            self, 'Select Source Dataset Directory')
         if dir != "":
             self.dataset_src_path_input_text.setText(str(dir))
-            self.slider_dataset.setCheckState(Qt.Checked) # workaround, for making sure, that the state changes
+            # workaround, for making sure, that the state changes
+            self.slider_dataset.setCheckState(Qt.Checked)
             self.slider_dataset.setCheckState(Qt.Unchecked)
 
     def openDestDatasetDirectorySelectDialog(self):
-        dir = QFileDialog.getExistingDirectory(self, 'Select Target Dataset Directory')
+        dir = QFileDialog.getExistingDirectory(
+            self, 'Select Target Dataset Directory')
         if dir != "":
             self.dataset_dest_path_input_text_2.setText(str(dir))
-            self.slider_dataset.setCheckState(Qt.Unchecked) # workaround, for making sure, that the state changes
+            # workaround, for making sure, that the state changes
+            self.slider_dataset.setCheckState(Qt.Unchecked)
             self.slider_dataset.setCheckState(Qt.Checked)
-
 
     def setFilesViewContent(self, path: str):
         model = DatasetFileModel()
@@ -209,7 +230,7 @@ class MyApp(QMainWindow, Ui_myApp):
         self.files_view.setRootIndex(model.index(path))
         self.files_view.hideColumn(1)
         self.files_view.hideColumn(3)
-        #resize columns
+        # resize columns
         for i in range(model.columnCount()):
             self.files_view.resizeColumnToContents(i)
 
@@ -291,7 +312,8 @@ class MyApp(QMainWindow, Ui_myApp):
                     if number_categories == 0:
                         number_categories = len(category_dirs)
                     else:
-                        raise Exception("Wrong directory structure, please use only a root directory and subdirectories for each category")
+                        raise Exception(
+                            "Wrong directory structure, please use only a root directory and subdirectories for each category")
                 number_files += len(files)
         except Exception:
             ErrorMessageBox(self).exec()
@@ -304,24 +326,33 @@ class MyApp(QMainWindow, Ui_myApp):
         if state > 0:
             dataset_dir = self.dataset_dest_path_input_text_2.text()
             if (dataset_dir == ""):
-                self.status_bar.showMessage("no source dataset directory selected! please select a parent directory, with subfolders for each category.", 5000)
+                self.status_bar.showMessage(
+                    "no source dataset directory selected! please select a parent directory, with subfolders for each category.", 5000)
                 return
         else:
             dataset_dir = self.dataset_src_path_input_text.text()
             if (dataset_dir == ""):
-                self.status_bar.showMessage("no destination dataset directory selected! please select a parent directory, with subfolders for each category.", 5000)
+                self.status_bar.showMessage(
+                    "no destination dataset directory selected! please select a parent directory, with subfolders for each category.", 5000)
                 return
         self.writeDatasetInfo(dataset_dir)
 
     def openAugmentationDialog(self):
         augmentations = {}
-        augmentations[AugmentationType.TRIM_SILENCE] = {"use": self.trim_silence_checkBox.isChecked(), "db_threshold": self.trim_silence_db_threshhold_dial.value()}
-        augmentations[AugmentationType.NORMALIZE_DURATION] = {"use": self.normalization_checkBox.isChecked(), "norm_duration_ms": self.normalization_dial.value()}
-        augmentations[AugmentationType.REPLICATION_FACTOR] = {"use": self.replication_checkBox.isChecked(), "factor": self.replication_dial.value()}
-        augmentations[AugmentationType.TIME_SHIFT] = {"use": self.time_shift_checkBox.isChecked(), "max_shift": self.time_shift_dial.value()}
-        augmentations[AugmentationType.PITCH] = {"use": self.pitch_checkBox.isChecked(), "semitones": self.pitch_dial.value()}
-        augmentations[AugmentationType.TIME_STRETCH] = {"use": self.time_stretch_checkBox.isChecked(), "min": self.time_stretch_min_dial.value(), "max": self.time_stretch_max_dial.value()}
-        augmentations[AugmentationType.VOLUME] = {"use": self.volume_checkBox.isChecked(), "volume_gain": self.volume_dial.value()}
+        augmentations[AugmentationType.TRIM_SILENCE] = {"use": self.trim_silence_checkBox.isChecked(
+        ), "db_threshold": self.trim_silence_db_threshhold_dial.value()}
+        augmentations[AugmentationType.NORMALIZE_DURATION] = {
+            "use": self.normalization_checkBox.isChecked(), "norm_duration_ms": self.normalization_dial.value()}
+        augmentations[AugmentationType.REPLICATION_FACTOR] = {
+            "use": self.replication_checkBox.isChecked(), "factor": self.replication_dial.value()}
+        augmentations[AugmentationType.TIME_SHIFT] = {
+            "use": self.time_shift_checkBox.isChecked(), "max_shift": self.time_shift_dial.value()}
+        augmentations[AugmentationType.PITCH] = {
+            "use": self.pitch_checkBox.isChecked(), "semitones": self.pitch_dial.value()}
+        augmentations[AugmentationType.TIME_STRETCH] = {"use": self.time_stretch_checkBox.isChecked(
+        ), "min": self.time_stretch_min_dial.value(), "max": self.time_stretch_max_dial.value()}
+        augmentations[AugmentationType.VOLUME] = {
+            "use": self.volume_checkBox.isChecked(), "volume_gain": self.volume_dial.value()}
 
         user_info = """
         Your audio records will be permanently changed by this operation, there is no way to undo the adjustments.\r\n
@@ -331,47 +362,98 @@ class MyApp(QMainWindow, Ui_myApp):
             try:
                 src_dataset_dir = self.dataset_src_path_input_text.text()
                 if (src_dataset_dir == ""):
-                    self.status_bar.showMessage("no source dataset directory selected! please select a parent directory, with subfolders for each category.", 5000)
+                    self.status_bar.showMessage(
+                        "no source dataset directory selected! please select a parent directory, with subfolders for each category.", 5000)
                     return
                 dest_dataset_dir = self.dataset_dest_path_input_text_2.text()
                 if (dest_dataset_dir == ""):
-                    self.status_bar.showMessage("no destination dataset directory selected! please select a parent directory, with subfolders for each category.", 5000)
+                    self.status_bar.showMessage(
+                        "no destination dataset directory selected! please select a parent directory, with subfolders for each category.", 5000)
                     return
                 file_paths = []
                 for root_dir, _, files in os.walk(src_dataset_dir):
                     for file in files:
-                        category_dir = re.search("\/[^\/]*$",root_dir).group()
+                        category_dir = re.search("\/[^\/]*$", root_dir).group()
                         file_paths.append(f"{category_dir}/{file}")
             except Exception:
                 ErrorMessageBox(self).exec()
-            
+
             if AugmentationDialog(self, file_paths, src_dataset_dir, dest_dataset_dir, augmentations).exec():
                 self.toggleDatasetDirectory(2)
-            
+
     # training page
+    def loadModelArchitectures(self):
+        architectures = None
+        try:
+            with open("model_size_info.yaml", "r") as config_yaml:
+                architectures = yaml.load(config_yaml, Loader=yaml.FullLoader)
+                config_yaml.close()
+        except Exception:
+            ErrorMessageBox(self).exec()
+        return architectures
+
     def openTrainingDatasetSelectDialog(self):
-        dir = QFileDialog.getExistingDirectory(self, 'Select Training Dataset Directory')
+        dir = QFileDialog.getExistingDirectory(
+            self, 'Select Training Dataset Directory')
         if dir != "":
             self.train_dataset_path_input.setText(str(dir))
-            words = self._readTrainingDatasetFolders(dir)
-            self.showTrainingWordsList(words)
+            self._words_to_train = self._readTrainingDatasetFolders(dir)
+            self.showTrainingWordsList(self._words_to_train)
 
     def openModelDestinationSelectDialog(self):
-        dir = QFileDialog.getExistingDirectory(self, 'Select Model Destination')
+        dir = QFileDialog.getExistingDirectory(
+            self, 'Select Model Destination')
         if dir != "":
             self.model_destination_input.setText(str(dir))
 
     def startTraining(self):
-        # TODO: implement training call
-        pass
+        try:
+            model = self.architectures[self.model_architecture_combobox.currentIndex(
+            )]
+            self.trainer = TrainingScriptWrapper(
+                textbox = self.training_log_textedit,
+                progressbar_epochs=self.progressBar_epochs,
+                progressbar_steps=self.progressBar_steps,
+                dataset_path = self.train_dataset_path_input.text(),
+                destination_path = self.model_destination_input.text(),
+                wanted_words = self._words_to_train,
+                audio_length_ms = str(self.audio_length_dial.value()),
+                dataset_silence_percentage = str(self.silence_percentage_dial.value()),
+                dataset_unknown_percentage = str(self.unknown_percentage_dial.value()),
+                mfcc_features = str(self.mfcc_features_dial.value()),
+                mfcc_window_size_ms = str(self.mfcc_window_size_dial.value()),
+                mfcc_window_stride_ms = str(self.mfcc_window_stride_dial.value()),
+                model_architecture = model['technical_name'],
+                model_size = model['model_size'],
+                sample_rate = str(self.sample_rate_dial.value())
+            )
+            self.stop_training_button.setEnabled(True)
+            self.trainer.start_process()
+        except Exception:
+            ErrorMessageBox(self).exec()
+
+    def stopTraining(self):
+        self.progressBar_epochs.setValue(0)
+        self.progressBar_epochs.setFormat("")
+        self.progressBar_steps.setFormat("")
+        self.progressBar_steps.setValue(0)
+        try:
+            if self.trainer is not None:
+                self.trainer.kill_process()
+                self.stop_training_button.setEnabled(False)
+        except Exception:
+            ErrorMessageBox(self).exec()
 
     def _readTrainingDatasetFolders(self, dir: str):
-        sub_folders = [name for name in os.listdir(dir) if os.path.isdir(os.path.join(dir, name))]
-        return sub_folders
+        try:
+            sub_folders = [name for name in os.listdir(
+                dir) if os.path.isdir(os.path.join(dir, name))]
+            return sub_folders
+        except Exception:
+            ErrorMessageBox(self).exec()
 
     def showTrainingWordsList(self, words):
         self.train_words_list.clear()
-        words.append("_silence_")
         words.sort()
         self.train_words_list.addItems(words)
 
@@ -381,6 +463,7 @@ def clearApplicationCache(workingDirectory: str):
     for f in os.listdir(dir):
         if f != ".gitkeep":
             os.remove(os.path.join(dir, f))
+
 
 if __name__ == "__main__":
     app = QApplication(sys.argv)
@@ -395,4 +478,3 @@ if __name__ == "__main__":
     except Exception:
         pass
     sys.exit()
-
---
+++ trainingScriptWrapper.py
@@ -0,0 +1,128 @@
+from PyQt5.QtWidgets import (QPlainTextEdit, QProgressBar,)                        
+from PyQt5.QtCore import QProcess
+from PyQt5.QtGui import QFont
+import re
+
+class TrainingScriptWrapper():
+
+    def __init__(self,
+                 textbox: QPlainTextEdit,
+                 progressbar_steps: QProgressBar,
+                 progressbar_epochs: QProgressBar,
+                 dataset_path: str,
+                 destination_path: str,
+                 wanted_words: list[str],
+                 sample_rate: str,
+                 audio_length_ms: str,
+                 dataset_unknown_percentage: str,
+                 dataset_silence_percentage: str,
+                 model_architecture: str,
+                 model_size: list[str],
+                 mfcc_features: str,
+                 mfcc_window_size_ms: str,
+                 mfcc_window_stride_ms: str
+                 ):
+        self.progressbar_steps = progressbar_steps
+        self.progressbar_epochs = progressbar_epochs
+        self.progressbar_steps.setValue(0)
+        self.progressbar_epochs.setValue(0)
+        # "unkwnown" dataset is collected from all other folders remaining, except the wanted words.
+        if "_unknown_" in wanted_words:
+            wanted_words.remove("_unknown_")
+        
+        # make a comma seperated list string
+        words_string = ','.join([str(item) for item in wanted_words]) 
+        self.script_args = ["/home/dsentler/ARM-software/ds_cnn/train.py",
+                            "--model_architecture", model_architecture,
+                            "--model_size_info"]
+        self.script_args.extend(model_size)
+        self.script_args.extend(["--dct_coefficient_count", mfcc_features,
+                                 "--window_size_ms", mfcc_window_size_ms,
+                                 "--window_stride_ms", mfcc_window_stride_ms,
+                                 "--learning_rate", "0.0005,0.0001,0.00002",
+                                 "--how_many_training_steps", "400,400,400",
+                                 "--summaries_dir", f"{destination_path}/{model_architecture}/retrain_logs",
+                                 "--train_dir", f"{destination_path}/{model_architecture}/training",
+                                 "--time_shift_ms", "0",
+                                 "--sample_rate", sample_rate,
+                                 "--clip_duration_ms", audio_length_ms,
+                                 "--wanted_words", words_string,
+                                 "--data_dir", dataset_path,
+                                 "--unknown_percentage", dataset_unknown_percentage,
+                                 "--silence_percentage", dataset_silence_percentage]
+                                 )
+        self.p = None
+        self.text = textbox
+        self.text.setFont(QFont("Courier New"))
+        self.text.setReadOnly(True)
+        self.text.clear()
+
+    def message(self, s: str):
+        # filter backspaces
+        if "\b" in s:
+            s = s.replace('\b', '')
+
+        lines = s.splitlines(keepends=False)
+        for line in lines:
+            if len(line) > 1:
+                parsed = self._parseOutputLine(line)
+                if not parsed:
+                    self.text.appendPlainText(line)
+
+    def _parseOutputLine(self, line : str) -> bool:
+        regex_epoch = 'Epoch ([\d]{1,3})/([\d]{1,3})\r?'
+        regex_steps = "[\s]{0,4}([\d]{1,3})/([\d]{1,3})\W*ETA: ([\d:smh]*)\W*loss: ([\d.]*)\W*accuracy: ([\d.]*)\r?"
+        match = re.match(regex_steps, line)
+        if match:
+            self.progressbar_steps.setValue(int(match[1]))
+            self.progressbar_steps.setMaximum(int(match[2]))
+            self.progressbar_steps.setFormat(f"Step: {match[1]}/{match[2]} - remaining: {match[3]} - loss: {match[4]} - acc: {match[5]}")
+            return True
+        match = re.match(regex_epoch, line)
+        if match:
+            self.progressbar_epochs.setValue(int(match[1]))
+            self.progressbar_epochs.setMaximum(int(match[2]))
+            self.progressbar_epochs.setFormat(f"Epoch: {match[1]}/{match[2]}")
+            return True
+
+        return False
+
+
+    def kill_process(self):
+        if self.p is not None:
+            self.p.kill()
+
+    def start_process(self):
+        if self.p is None:  # No process running.
+            self.message("Executing process")
+            # Keep a reference to the QProcess (e.g. on self) while it's running.
+            self.p = QProcess()
+            self.p.readyReadStandardOutput.connect(self.handle_stdout)
+            self.p.readyReadStandardError.connect(self.handle_stderr)
+            self.p.stateChanged.connect(self.handle_state)
+            # Clean up once complete.
+            self.p.finished.connect(self.process_finished)
+            self.p.start("python3", self.script_args)
+
+    def handle_stderr(self):
+        data = self.p.readAllStandardError()
+        stderr = bytes(data).decode("utf8")
+        self.message(stderr)
+
+    def handle_stdout(self):
+        data = self.p.readAllStandardOutput()
+        stdout = bytes(data).decode("utf8")
+        self.message(stdout)
+
+    def handle_state(self, state):
+        states = {
+            QProcess.NotRunning: 'Not running',
+            QProcess.Starting: 'Starting',
+            QProcess.Running: 'Running',
+        }
+        state_name = states[state]
+        self.message(f"State changed: {state_name}")
+
+    def process_finished(self):
+        self.message("Process finished.")
+        self.p = None
